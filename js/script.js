57be1d855cf118a8dd1355c2d45abe277a8a2d56
DavidZarfati <zarda2001@gmail.com>
2025-11-19 12:48:34 +0100

    feat:sviluppo esercizio

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 const fotoGrid = document.querySelector(".foto-grid")
 
+
 function myCreateElement4(
     tagnName,
     classList = [],
@@ -36,7 +37,7 @@ function myCreateElement4(
 }
 
 function createPhotoCards(fotoArray) {
-    fotoArray.forEach(curFoto => {
+    fotoArray.forEach((curFoto, indice) => {
         const card4 = myCreateElement4("div", ["card"], [
             // card-image con pin e foto
             myCreateElement4("div", ["card-image"], [
@@ -47,8 +48,12 @@ function createPhotoCards(fotoArray) {
                 myCreateElement4("img", ["img-fluid"], [], (el) => {
                     el.src = curFoto.url;
                     el.alt = "Immagine foto";
+                    // Aggiungo dataset direttamente alla creazione
+                    el.dataset.id = indice;
+                    el.dataset.url = curFoto.url;
                 })
             ]),
+
 
             // card-title con il div e l' h2 dentro
             myCreateElement4("div", ["card-title"], [
@@ -56,16 +61,42 @@ function createPhotoCards(fotoArray) {
                 myCreateElement4("h2", [], curFoto.title)
             ])
         ]);
-
+        card4.dataset.url = curFoto.url
         // attacco ogni card al fotoGrid
         fotoGrid.appendChild(card4);
     });
 }
 
+//   const carte = document.querySelectorAll("card")
+// console.log(carte);
+const overlay = document.querySelector(".copritutto");
+const fotoModale = document.querySelector(".foto-modale");
+const btnChiudi = document.querySelector(".btn-chiudi");
+
+btnChiudi.addEventListener("click", function () {
+    overlay.classList.add("nascosto");
+});
+overlay.addEventListener("click", function () {
+    overlay.classList.add("nascosto");
+});
+fotoModale.addEventListener("click", function () {
+    event.stopPropagation();
+});
+
+
+
 axios.get("https://lanciweb.github.io/demo/api/pictures/")
     .then(function (resp) {
         const fotoArray = resp.data;
         createPhotoCards(fotoArray);
+        const scorriFoto = document.querySelectorAll(".card");
+
+        scorriFoto.forEach((foto) => {
+            foto.addEventListener("click", function () {
+                overlay.classList.remove("nascosto");
+                fotoModale.innerHTML = `<img src="${this.dataset.url}" alt="Foto">`;
+            });
+        });
     })
 
 
@@ -86,6 +117,27 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+//   const carte = document.querySelectorAll("card")
+// console.log(carte);
+// ...existing code...
+// fotoGrid.addEventListener("click", function (event) {
+// if (event.target.classList.contains("img-fluid")) {
+//     divNascosto.classList.remove("nascosto");
+//     fotoModale.innerHTML = `<img src="${event.target.src}">`;
+// }
+// });
 // Versione prima di creare createmyelement4
 //         // console.log(resp); debug
 //         const fotoArray = resp.data
